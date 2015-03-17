5bc729e26c22c0229b24e33a47a32334fa9f737a
KrishnaIndia <krishnaof1988@gmail.com>
2015-03-17 11:26:12 +0530

    build script modified to check for sodium library locally. If not available then check from lib folder in the root

--- libsodium-sys/build.rs
+++ libsodium-sys/build.rs
@@ -1,3 +1,16 @@
+extern crate "pkg-config" as pkg_config;
+
+use std::env;
+
+fn find_static_lib(lib_name: &str) -> bool {
+	match pkg_config::Config::new().statik(true).find(lib_name) {
+			Ok(_) => true,
+			Err(_) => false
+		}
+}
+
 fn main () {
-    println!("cargo:rustc-flags=-l sodium");
+  if !find_static_lib("sodium") {
+    println!("cargo:rustc-flags=-l sodium -L lib/");
+  }
 }
