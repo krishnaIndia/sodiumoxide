f98f2d9ef5e45eb0f76fb3f54b849e91a48ac621
KrishnaIndia <krishnaof1988@gmail.com>
2015-03-17 13:06:01 +0530

    lib folder changed to third_party_libs

--- libsodium-sys/build.rs
+++ libsodium-sys/build.rs
@@ -1,16 +1,14 @@
 extern crate "pkg-config" as pkg_config;
 
-use std::env;
-
 fn find_static_lib(lib_name: &str) -> bool {
-	match pkg_config::Config::new().statik(true).find(lib_name) {
-			Ok(_) => true,
-			Err(_) => false
-		}
+  match pkg_config::Config::new().statik(true).find(lib_name) {
+    Ok(_) => true,
+    Err(_) => false
+  }
 }
 
 fn main () {
   if !find_static_lib("sodium") {
-    println!("cargo:rustc-flags=-l sodium -L lib/");
+    println!("cargo:rustc-flags=-l sodium -L third-party-libs/");
   }
 }
